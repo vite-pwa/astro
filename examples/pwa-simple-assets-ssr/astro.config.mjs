5d9b26fc57d4ad8be9df8a63a89bd0f855735c1f
Hugo van Rijswijk <git@hugovr.nl>
2025-04-02 16:47:27 +0200

    chore: add SSR example

---
+++ examples/pwa-simple-assets-ssr/astro.config.mjs
@@ -0,0 +1,51 @@
+import node from '@astrojs/node'
+import AstroPWA from '@vite-pwa/astro'
+import { defineConfig } from 'astro/config'
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    logLevel: 'info',
+    define: {
+      __DATE__: `'${new Date().toISOString()}'`,
+    },
+    server: {
+      fs: {
+        // Allow serving files from hoisted root node_modules
+        allow: ['../..'],
+      },
+    },
+  },
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+  integrations: [
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      includeAssets: ['favicon.svg'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Astro PWA',
+        short_name: 'Astro PWA',
+        theme_color: '#ffffff',
+      },
+      pwaAssets: {
+        config: true,
+      },
+      workbox: {
+        navigateFallback: '/',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\/$/],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+    }),
+  ],
+})
